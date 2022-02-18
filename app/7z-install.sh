7f5aa94ed15e1fbe71c6e6275692ae8deb2b1d21
Victor Sanikovich <sanikovich@gmail.com>
2022-02-19 01:54:55 +0300

    Upd

--- app/7z-install.sh
+++ app/7z-install.sh
@@ -3,7 +3,8 @@
     my_file=7z2107-linux-x64
     # read -p "Press enter to continue"
     wget https://www.7-zip.org/a/$my_file.tar.xz
-    tar -xzf $my_file.tar.xz
+    7za x $my_file.tar.xz
+    tar -xzf $my_file.tar
     cd $my_file/
     ./configure &
     process_id=$!
@@ -17,5 +18,6 @@
     cd ..
     rm -rf $my_file/
     rm -r $my_file.tar.xz
+    rm -r $my_file.tar
 fi
 # end
