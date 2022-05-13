05d59b66b147d651db4a5847534ce639caaf1190
Victor Sanikovich <sanikovich@gmail.com>
2022-05-14 02:50:25 +0300

    up

--- kernel5.sh
+++ kernel5.sh
@@ -5,4 +5,5 @@ yum -y install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 yum -y --enablerepo=elrepo-kernel install kernel-ml
 grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
+#yum -y versionlock kernel-[0-9]*
 reboot
