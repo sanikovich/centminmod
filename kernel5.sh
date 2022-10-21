7f9b767c3f102e9db396aeac9531b9d43b79a163
Victor Sanikovich <sanikovich@gmail.com>
2022-10-21 20:21:34 +0300

    Kernel 6

--- kernel5.sh
+++ kernel5.sh
@@ -4,6 +4,7 @@ rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 yum -y install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 yum -y --enablerepo=elrepo-kernel install kernel-ml
 grub2-set-default 0
+echo "GRUB_DEFAULT=0" >> /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
 #yum -y versionlock kernel-[0-9]*
 reboot
