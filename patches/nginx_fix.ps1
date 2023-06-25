8fc44a5f108f67db547254172a8a2e776b697ec1
Victor Sanikovich <sanikovich@gmail.com>
2023-06-25 20:40:12 +0300

    Patches Dir

---
+++ patches/nginx_fix.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+    Fix problem with http2 parameter deprecation
+.DESCRIPTION
+    Find string:
+                listen 443 ssl http2;"
+    and replace:
+                listen 443 ssl;
+                http2 on;
+.NOTES
+  Version : 0.0.1 -
+
+#>
+[regex]$varFind = "([^\S\r\n])*listen 443 ssl http2;"
+$varReplace = @"
+  listen 443 ssl;
+  http2 on;
+"@
+
+$dirPath = "/usr/local/nginx/conf/conf.d"
+$fileList = Get-ChildItem -Path $dirPath -Include "*.ssl.conf" -Force -File -Depth 0
+$fileList | ForEach-Object {
+    $(Get-Content -Path $_.FullName) -Replace($varFind,$varReplace) | Out-File -FilePath $_.FullName
+}
